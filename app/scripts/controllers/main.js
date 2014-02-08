3d5f356c96d884b9244a171fcdd1cd791cf245ee
Brandon Jackson <brandonscottjackson@gmail.com>
2014-02-08 12:53:04 -0500

    initialized with Yeoman Angular gen

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cloudtapesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
