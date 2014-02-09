271c45d0c6e177b7059dce91999c19b9a2bbcb6c
Seth Thompson <s3th.thompson@gmail.com>
2014-02-09 12:45:57 -0500

    scaffold mix and track models

---
+++ app/scripts/models/track.js
@@ -0,0 +1,19 @@
+/*global ss14Team45, Backbone*/
+
+var TrackModel= Backbone.Model.extend({
+
+    url: '',
+
+    initialize: function() {
+    },
+
+    defaults: {
+    },
+
+    validate: function(attrs, options) {
+    },
+
+    parse: function(response, options)  {
+        return response;
+    }
+});
