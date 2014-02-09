b5221311ead7eeac7efe411ddec831efc0a15f8c
Seth Thompson <s3th.thompson@gmail.com>
2014-02-09 12:42:00 -0500

    copy Dropbox code from angular app

---
+++ app/scripts/lib/DropboxClient.js
@@ -0,0 +1,77 @@
+// DropboxClient
+
+var DropboxClient = (function(){
+
+  var DropboxClient = function(key, receiverURL){
+
+    this.client = new Dropbox.Client({
+      key: key
+    });
+
+    this.client.authDriver(
+      new Dropbox.AuthDriver.Popup({
+        receiverUrl: receiverURL
+      })
+    );
+  };
+
+  DropboxClient.prototype.authenticate = function(){
+    var deferred = Q.defer();
+    this.client.authenticate(function(error, client){
+      if(error){
+        console.log("DropboxClient.authenticate() error");
+        deferred.reject(error);
+      } else {
+        console.log("DropboxClient.authenticate() success");
+        deferred.resolve(client);
+      }
+    });
+    return deferred.promise;
+  };
+
+  DropboxClient.prototype.mkdir = function(path){
+    var deferred = Q.defer();
+    this.client.mkdir(path, function(error, stat){
+      if(error){
+        console.log("DropboxClient.mkdir() error");
+        deferred.reject(error);
+      } else {
+        console.log("DropboxClient.mkdir() success");
+        deferred.resolve(stat);
+      }
+    });
+    return deferred.promise;
+  }
+
+
+  DropboxClient.prototype.writeFile = function(path, data){
+    var deferred = Q.defer();
+    this.client.writeFile(path, data, function(error, stat){
+      if(error){
+        console.log("DropboxClient.writeFile()) error");
+        deferred.reject(error);
+      } else {
+        console.log("DropboxClient.writeFile()) success");
+        deferred.resolve(stat);
+      }
+    });
+    return deferred.promise;
+  }
+
+  DropboxClient.prototype.makeUrl = function(path, params){
+    var deferred = Q.defer();
+    this.client.makeUrl(path, params, function(error, urlObject){
+      if(error){
+        console.log("DropboxClient.makeUrl() error");
+        deferred.reject(error);
+      } else {
+        console.log("DropboxClient.makeUrl() success");
+        deferred.resolve(urlObject);
+      }
+    });
+    return deferred.promise;
+  }
+
+  return DropboxClient;
+
+}());
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,4 +15,25 @@ window.ss14Team45 = {
 $(document).ready(function () {
     'use strict';
     ss14Team45.init();
+
+    var client = new DropboxClient("f6u02e6s8nett1d", "http://localhost/cloudtapes/app/receiver.html");
+
+    client.authenticate()
+        .then(function(client){
+          return client.mkdir("cirrus");
+        })
+        .then(function(stat){
+          return client.writeFile("cirrus/hello_world.txt", "Hello, world!\n");
+        })
+        .then(function(stat){
+          return client.makeUrl("cirrus",{ long: true });
+        })
+        .then(function(urlObject){
+          console.log(urlObject);
+          alert("URL: "+urlObject.url+"?dl=1");
+        })
+        .fail(function(error){
+          console.log(error);
+          alert("An Unexpected Error Has Occurred");
+        });
 });
