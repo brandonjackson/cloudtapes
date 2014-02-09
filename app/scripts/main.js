2d7381f935a21b695ba2a26d1a77c0e0a390abba
Seth Thompson <s3th.thompson@gmail.com>
2014-02-09 12:31:07 -0500

    fuck angular, commence backbone hailmary

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global ss14Team45, $*/
+
+
+window.ss14Team45 = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    ss14Team45.init();
+});
